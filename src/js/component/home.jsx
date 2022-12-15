16fa3c19c888d70867df8905c7f43c3d4631685c
Andres Longueira <andreslongueirarengifo@gmail.com>
2022-12-15 21:32:23 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TodoList from "./todoList.jsx";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+	const [data, setData] = useState([])
+	const ref = useRef(null);
+
+	const HandleTasks = (ev)=>{
+		if (ev.key === "Enter"){	
+			setData(data.concat([{"label":ev.target.value,"done":false}]))
+			ref.current.value = '';
+		}
+	}
+
+	//get data
+	useEffect(() => {
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/l_test`)
+		  .then((response) => {
+			if (!response.ok) {
+			  throw new Error(
+				`This is an HTTP error: The status is ${response.status}`
+			  );
+			}
+			return response.json();
+		  })
+		  .then((actualData) => setData(actualData))
+		  .catch((err) => {
+			console.log(err.message);
+		  });
+	  }, []);
+
+
+	//post data
+	fetch('https://assets.breatheco.de/apis/fake/todos/user/l_test', {
+		method: "PUT",
+		body: JSON.stringify(data),
+		headers: {
+		  "Content-Type": "application/json"
+		}
+	  })
+	  .then(resp => {
+		  console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+		  console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+		  console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+		  return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+	  })
+	  .then(actualData => {
+		  //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+		  console.log(actualData); //esto imprimirá en la consola el objeto exacto recibido del servidor
+	  })
+	  .catch(error => {
+		  //manejo de errores
+		  console.log(error);
+	  });
+
+	  
+	  return (
+		  <div className="d-flex justify-content-center flex-column aling-items-center">
+		  <h1>TODO LIST</h1>
+		  <input ref={ref} type="text" onKeyUp={HandleTasks} name="" id="" className="w-25" />
+		  <TodoList data={data} setter={setData}/>
+		  </div>
+  
+	  );
 };
 
 export default Home;
---
+++ src/js/component/todoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+
+
+function TodoList(prop) {
+
+    const HandleRemoveTask=(index)=>{
+        prop.setter(prop.data.filter((e,currentIndex)=>index!=currentIndex)) 
+    }
+
+     const list = prop.data.map((e,i)=>{
+             return (<ListGroup.Item className='d-flex justify-content-between'>{e.label} <Button variant="danger" onClick={()=>HandleRemoveTask(i)}><i className='fa-solid fa-trash'></i></Button> </ListGroup.Item>)
+         }) 
+
+  return (
+    <ListGroup>
+      {list}
+    </ListGroup>
+  );
+}
+
+export default TodoList;
